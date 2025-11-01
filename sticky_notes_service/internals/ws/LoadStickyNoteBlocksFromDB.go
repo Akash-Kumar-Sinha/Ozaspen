0c06d5d85ef398ef0e8d53adbd283751fa19f6ad
Akash-Kumar-Sinha <akashkumarsinha304@gmail.com>
2025-11-02 04:20:10 +0530

    add load sticky note function

---
+++ sticky_notes_service/internals/ws/LoadStickyNoteBlocksFromDB.go
@@ -0,0 +1,31 @@
+package ws
+
+import (
+	"fmt"
+	"sticky_notes_service/internals/database"
+	savestickynotes "sticky_notes_service/internals/handlers/SaveStickyNotes"
+	"sticky_notes_service/internals/helpers"
+	"sticky_notes_service/internals/models"
+
+	"gorm.io/datatypes"
+)
+
+func LoadStickyNoteBlocksFromDB(stickyNoteID string, client *Client) (datatypes.JSON, error) {
+	stickyNoteUUID, err := helpers.ParseUuid(stickyNoteID)
+	if err != nil {
+		return nil, fmt.Errorf("invalid sticky note ID: %v", err)
+	}
+
+	if _, allowed, err := savestickynotes.CheckPermission(stickyNoteUUID, client.profileID); err != nil || !allowed {
+		return nil, fmt.Errorf("permission check failed: %v", err)
+	}
+
+	var stickyNote models.StickyNote
+	if err := database.DB.
+		Preload("Content").
+		First(&stickyNote, "id = ?", stickyNoteUUID).Error; err != nil {
+		return nil, fmt.Errorf("error loading sticky note from DB: %v", err)
+	}
+
+	return stickyNote.Content.Blocks, nil
+}
